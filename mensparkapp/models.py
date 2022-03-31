b5a29a762a58ec7af95a2da210ec501d75d7fe9d
Akm Kawser <kawserrabbi3@gmail.com>
2022-04-01 03:49:46 +0600

    created superuser and admin.py file for accessing models from admin area

--- mensparkapp/models.py
+++ mensparkapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product (models.Model):
+    product_name = models.CharField(max_length=100)
+    product_img = models.ImageField(upload_to = 'pics/product')
+    product_price = models.IntegerField()
+
+class Outlet (models.Model):
+    outlet_name = models.CharField (max_length=100)
+    address = models.TextField()
+    phone = models.IntegerField()
+    manager_name = models.CharField(max_length=100)
+    opentime = models.TimeField()
+    closetime = models.TimeField()
+    outlet_img = models.ImageField(upload_to = 'pics/outlet')
